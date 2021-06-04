d30077ae23a3fa17a27e2b5d59d486747c72e729
eliasn <nebiyuelias1@gmail.com>
2021-06-04 09:01:55 +0300

    Implement creation modal

--- gebeya_ai_project/urls.py
+++ gebeya_ai_project/urls.py
@@ -13,13 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='views/index.html'), name='index'),
     path('admin/', admin.site.urls),
     path('views/', include('views.urls')),
     path('accounts/', include('allauth.urls')),
-    path('', TemplateView.as_view(template_name='views/index.html'), name='index'),
+    path('avatar/', include('avatar.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
