1fbd135db5c9857de414bd5e628b5de3166cec5d
eliasn <nebiyuelias1@gmail.com>
2021-06-10 08:21:29 +0300

    Work on Admin interface for views

--- views/urls.py
+++ views/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from views.views import create_view, index
+from views.views import create_view, index, detail
 
 urlpatterns = [
     path('', index, name='index'),
     path('create/', create_view, name='create_view'),
+    path('detail/<int:pk>/', detail, name='view_detail')
 ]
